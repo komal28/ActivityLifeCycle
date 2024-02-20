c96dc551702097abdbde05b975ff8f21971f25fe
komal1101peoplesuite <komal.shinde@peoplesuitehcm.co.in>
2024-02-20 09:47:18 +0530

    Activity Lifecycle with Jetpack Compose

--- app/src/main/java/com/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/activitylifecycle/MainActivity.kt
@@ -27,11 +27,41 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
         Toast.makeText(applicationContext, "onCreate Called", Toast.LENGTH_LONG).show()
+    }
 
+
+    override fun onStart() {
+        super.onStart()
+        val toast = Toast.makeText(applicationContext, "onStart Called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        val toast = Toast.makeText(applicationContext, "onRestart Called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val toast = Toast.makeText(applicationContext, "onPause Called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val toast = Toast.makeText(applicationContext, "onResume Called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val toast = Toast.makeText(applicationContext, "onStop Called", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val toast = Toast.makeText(applicationContext, "onDestroy Called", Toast.LENGTH_LONG).show()
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
